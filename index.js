b1ba1575f7d7cc00886707f17cd4e6d213c5201c
CrispusDH <qacrispus@gmail.com>
2017-06-25 17:12:09 +0300

    convert index file from .ts to .js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 module.exports = function (ptor) {
     ptor.by.addLocator('dataTestId',
-        function (dataTestId: string, parentElement: HTMLElement | null | Document) {
+        function (dataTestId, parentElement) {
         parentElement = parentElement || document;
-        let nodes = parentElement.querySelectorAll('[data-test-id]');
+        var nodes = parentElement.querySelectorAll('[data-test-id]');
         return Array.prototype.filter.call(nodes, function(node) {
             return (node.getAttribute('data-test-id') === dataTestId);
         });
