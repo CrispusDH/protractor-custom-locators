e04f1e723b18abb23db5272267c167cb19c6837b
CrispusDH <qacrispus@gmail.com>
2017-06-25 16:29:46 +0300

    remove extra let

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 module.exports = function (ptor) {
     ptor.by.addLocator('dataTestId',
         function (dataTestId: string, parentElement: HTMLElement | null | Document) {
-        let parentElement = parentElement || document;
+        parentElement = parentElement || document;
         let nodes = parentElement.querySelectorAll('[data-test-id]');
         return Array.prototype.filter.call(nodes, function(node) {
             return (node.getAttribute('data-test-id') === dataTestId);
