add9e93d98a8beae09c7870b9285e3ca3ead729a
CrispusDH <qacrispus@gmail.com>
2017-07-05 11:13:26 +0300

    refactor for ability to use several custom locators

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 'use strict';
 
-module.exports = function (ptor) {
+const dataTestId = function (ptor) {
     ptor.by.addLocator('dataTestId',
         function (dataTestId, parentElement) {
-        var using = parentElement || document;
-        var nodes = using.querySelectorAll('[data-test-id]');
-        return Array.prototype.filter.call(nodes, function(node) {
-            return (node.getAttribute('data-test-id') === dataTestId);
+            let using = parentElement || document;
+            let nodes = using.querySelectorAll('[data-test-id]');
+            return Array.prototype.filter.call(nodes, function (node) {
+                return (node.getAttribute('data-test-id') === dataTestId);
+            });
         });
-    });
+};
 
+const dataReactId = function (ptor) {
     ptor.by.addLocator('dataReactId',
         function (dataReactId, parentElement) {
-            var using = parentElement || document;
-            var nodes = using.querySelectorAll('[data-reactid]');
+            let using = parentElement || document;
+            let nodes = using.querySelectorAll('[data-reactid]');
             return Array.prototype.filter.call(nodes, function(node) {
                 return (node.getAttribute('data-test-id') === dataTestId);
             });
         })
 };
+
+const addCustomLocator = {
+    dataTestId: dataTestId(),
+    dataReactId: dataReactId()
+};
+
+export default addCustomLocator;
