56a5f428aca44c0adda230f326deff568194248a
CrispusDH <qacrispus@gmail.com>
2017-07-06 13:08:07 +0300

    fix inside method v2.5

--- index.ts
+++ index.ts
@@ -2,50 +2,34 @@ import {Ptor} from "protractor";
 
 export default class AddCustomLocators {
 
-    static createCustomLocator(protractor: Ptor, locatorName: string) {
+    static createCustomLocator(protractor: Ptor, locatorName: string, locatorAttribute: string) {
         console.log('1................................................');
-    protractor.by.addLocator(locatorName,
-        function (expected, parentElement: HTMLElement | null) {
-            console.log('2................................................');
+        let script = function (expected, parentElement: HTMLElement | null) {
+            console.log('2................................................', locatorAttribute);
             let using = parentElement || document;
-            let nodes = using.querySelectorAll('[data-reactid]');
+            let nodes = using.querySelectorAll(`[${locatorAttribute}]`);
             return Array.prototype.filter.call(nodes, function (node) {
                 console.log('3................................................');
-                return (node.getAttribute('data-reactid') === expected);
+                return (node.getAttribute(`${locatorAttribute}`) === expected);
             });
-        });
+        };
+    protractor.by.addLocator(locatorName,
+        script);
 };
 
 /*
- 'use strict';
-
- const dataTestId = function (ptor) {
- ptor.by.addLocator('dataTestId',
- function (dataTestId, parentElement) {
- let using = parentElement || document;
- let nodes = using.querySelectorAll('[data-test-id]');
- return Array.prototype.filter.call(nodes, function (node) {
- return (node.getAttribute('data-test-id') === dataTestId);
- });
- });
- };
-
- const dataReactId = function (ptor) {
- ptor.by.addLocator('dataReactId',
- function (dataReactId, parentElement) {
+ static createCustomLocator(protractor: Ptor, locatorName: string) {
+ console.log('1................................................');
+ protractor.by.addLocator(locatorName,
+ function (expected, parentElement: HTMLElement | null) {
+ console.log('2................................................');
  let using = parentElement || document;
  let nodes = using.querySelectorAll('[data-reactid]');
- return Array.prototype.filter.call(nodes, function(node) {
- return (node.getAttribute('data-test-id') === dataTestId);
+ return Array.prototype.filter.call(nodes, function (node) {
+ console.log('3................................................');
+ return (node.getAttribute('data-reactid') === expected);
  });
- })
+ });
  };
-
- const addCustomLocator = {
- dataTestId: dataTestId,
- dataReactId: dataReactId
- };
-
- export default addCustomLocator;
  */
 }
