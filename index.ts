ca93db312fd9c57b0cd0cd5bb6e03f0ec5922fd8
CrispusDH <qacrispus@gmail.com>
2017-07-06 12:43:15 +0300

    fix inside method v2.4

--- index.ts
+++ index.ts
@@ -3,11 +3,14 @@ import {Ptor} from "protractor";
 export default class AddCustomLocators {
 
     static createCustomLocator(protractor: Ptor, locatorName: string) {
+        console.log('1................................................');
     protractor.by.addLocator(locatorName,
         function (expected, parentElement: HTMLElement | null) {
+            console.log('2................................................');
             let using = parentElement || document;
             let nodes = using.querySelectorAll('[data-reactid]');
             return Array.prototype.filter.call(nodes, function (node) {
+                console.log('3................................................');
                 return (node.getAttribute('data-reactid') === expected);
             });
         });
